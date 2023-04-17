b9ffefe43e30368751ca58cb268c8d48ebb11612
Hadjer Attatba <archiger@yahoo.fr>
2023-04-17 06:03:47 +0200

    phase2.py

--- phase2.py
+++ phase2.py
@@ -1,19 +1,17 @@
-
 import requests
 from requests import get
 from bs4 import BeautifulSoup
-
-def get_categories():
-    response = get("http://books.toscrape.com/index.html")
-    soup = BeautifulSoup(response.text, "html.parser")
-    aside = soup.find('aside')
-    links = aside.find_all('a')
-    links_title = [link.text.strip() for link in links]
-    links_href = [link["href"] for link in links]
-    return links_title, links_href
-print(get_categories())
-# Choix d'une categorie:
-url_categorie ="https://books.toscrape.com/catalogue/category/books/add-a-comment_18/index.html"
+url = "http://books.toscrape.com/index.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+aside = soup.find('aside')
+links = aside.find_all('a')
+links_title = [link.text.strip() for link in links]
+links_href = [link["href"] for link in links]
+classics_category = links_title[5]
+print(links_title)
+print(classics_category)
+url_categorie ="http://books.toscrape.com/catalogue/category/books/classics_6/index.html"
 def scrap_categorie(url):
     book_urls =[]
     r= requests.get(url_categorie)
@@ -26,32 +24,22 @@ def scrap_categorie(url):
         book_url = article.find("a")["href"][8:]
         book_url=f"https://books.toscrape.com/catalogue{book_url}"
         print(book_url)
+    # récuperer le titre d'un livre
+        titre_livre = article.find("h3").text
+        print(titre_livre)
     #Ajouter url du livre a la liste book_urls
         book_urls.append(book_url)
-    # Afficher la premiere page
-        page = soup.select_one("li.current").text.strip('../../')
-        print(page)
-    #integrer le bouton page suivante
-        next_page = soup.select_one("li.next>a")["href"]
-        # si il ya une page suivante scrap les données sinon stop
+    #Ajouter le titre du livre :
+        book_urls.append(titre_livre)
     return book_urls
 # Créer un nouveau fichier pour écrire dans le fichier appelé « phase2.csv »
-    with open(f"{url_categorie}.csv","a") as file: file.write(information)
-    en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_exluding_tax", "number_available",
-               "product_description", "category", "review_rating", "image_url"]
+    en_tete = ["book_urls"]
     # Créer un nouveau fichier pour écrire dans le fichier appelé « phase1.csv »
     with open('phase2.csv', 'w') as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=',')
         writer.writerow(en_tete)
-        ligne = [product_page_url, upc, title, price_including_tax, price_exluding_tax, number_available,
-                 product_description,
-                 category, review_rating, image_url]
+        ligne = [links_title]
         writer.writerow(ligne)
-    return product_page_url, upc, title, price_including_tax, price_exluding_tax, number_available, product_description, category
+    return books_urls, links_title
 
 print(scrap_categorie(url_categorie))
-
-
-
-
-
