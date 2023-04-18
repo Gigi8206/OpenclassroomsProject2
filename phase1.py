d37cd0614cd0011f8ca893a5fd8c6fdfe003da23
Hadjer Attatba <archiger@yahoo.fr>
2023-04-18 10:19:18 +0200

    phase1.py

--- phase1.py
+++ phase1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 def scrap_book():
+ main_page = "http://books.toscrape.com/"
  url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
  reponse = requests.get(url)
  page = reponse.content
@@ -18,7 +19,7 @@ def scrap_book():
  list_a = soup.findAll('a')
  category = list_a[3].text
  review_rating = soup.find("p",{"class": "star-rating"}).get("class")[1]
- image_url = soup.find("img").get("src").strip('../../')
+ image_url = main_page+soup.find("img").get("src").strip('../../')
  en_tete = ["product_page_url", "upc", "title", "price_including_tax", "price_exluding_tax", "number_available",
             "product_description", "category", "review_rating", "image_url"]
  # Créer un nouveau fichier pour écrire dans le fichier appelé « phase1.csv »
@@ -30,6 +31,4 @@ def scrap_book():
   writer.writerow(ligne)
  return product_page_url, upc, title, price_including_tax,price_exluding_tax,number_available,product_description,category,review_rating,image_url
 print(scrap_book())
-
-
 
