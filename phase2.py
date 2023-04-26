92e766665aa49393087980c7d5a32a0828f75e9e
Hadjer Attatba <archiger@yahoo.fr>
2023-04-26 16:01:28 +0100

    Mise a jour code

--- phase2.py
+++ phase2.py
@@ -1,45 +1,53 @@
-import requests
+from phase1 import scrap_book
 from requests import get
 from bs4 import BeautifulSoup
-url = "http://books.toscrape.com/index.html"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-aside = soup.find('aside')
-links = aside.find_all('a')
-links_title = [link.text.strip() for link in links]
-links_href = [link["href"] for link in links]
-classics_category = links_title[5]
-print(links_title)
-print(classics_category)
-url_categorie ="http://books.toscrape.com/catalogue/category/books/classics_6/index.html"
-def scrap_categorie(url):
-    book_urls =[]
-    r= requests.get(url_categorie)
-    if r.ok:
-        soup = BeautifulSoup(r.content,"html.parser")
-    else:print()
-    articles = soup.find_all("article", class_ = "product_pod")
-    for article in articles:
-    # récuperer url d'un livre
-        book_url = article.find("a")["href"][8:]
-        book_url=f"https://books.toscrape.com/catalogue{book_url}"
-        print(book_url)
-    # récuperer le titre d'un livre
-        titre_livre = article.find("h3").text
-        print(titre_livre)
-    #Ajouter url du livre a la liste book_urls
-        book_urls.append(book_url)
-    #Ajouter le titre du livre :
-        book_urls.append(titre_livre)
-    return book_urls
-# Créer un nouveau fichier pour écrire dans le fichier appelé « phase2.csv »
-    en_tete = ["book_urls"]
-    # Créer un nouveau fichier pour écrire dans le fichier appelé « phase1.csv »
-    with open('phase2.csv', 'w') as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=',')
-        writer.writerow(en_tete)
-        ligne = [links_title]
-        writer.writerow(ligne)
-    return books_urls, links_title
+from csv import writer
 
-print(scrap_categorie(url_categorie))
+root_url = "http://books.toscrape.com/"
+
+def upload_books(book_titles, books_data):
+    with open("phase2.csv", "w", newline="") as csvfile:
+        book_writer = writer(csvfile)
+        book_writer.writerow( book_titles)
+        book_writer.writerows(books_data)
+def get_categories():
+    response = get("http://books.toscrape.com/index.html")
+    soup = BeautifulSoup(response.text, "html.parser")
+    nav = soup.find('ul', {"class": "nav nav-list"})
+    links = nav.find_all('a')
+    categories = [(link.text.strip(), root_url + link["href"]) for link in links]
+    return categories
+
+def get_user_category(titles):
+    print(f"Choisissez entre { ', '.join(titles) }")
+    user_response = input("Selectionnez votre categorie : ")
+    return user_response
+def get_books_by_category():
+    categories = get_categories()
+    print(f"{ len(categories) - 1} categories trouvées")
+    titles = [category[0] for category in categories[1:]]
+    user_response = get_user_category(titles)
+    while user_response not in titles:
+        print("Mauvaise catégorie")
+        user_response = get_user_category(titles)
+
+    books_data = []
+    for title, href in categories:
+        if user_response == title:
+            response = get(href)
+            soup = BeautifulSoup(response.text, "html.parser")
+            articles = soup.find_all('article')
+            page = (soup.find("li", {"class": "current"}))
+            # verifier si,il n'ya pas une autre page :
+            if page is None:
+                all_h3 = soup.find_all('h3').text
+            links = [article.find('a') for article in articles]
+            for link in links:
+                book_titles, book_data = scrap_book( "http://books.toscrape.com/catalogue/" + link["href"].strip('../../../'))
+                books_data.append(book_data)
+    return book_titles, books_data
+
+if __name__ == '__main__':
+        book_titles, books_data = get_books_by_category()
+        breakpoint()
+        upload_books(book_titles, books_data)
